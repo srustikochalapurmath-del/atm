a893055a35d122d8d716ebbd4d6592b5240a2f99
srustikochalapurmath <srustikochalapurmath@gmail.com>
2025-11-19 11:21:38 +0530

    commited

--- atm.py
+++ atm.py
@@ -1,27 +1,32 @@
 import sys
-if len(sys.argv) != 4:
-    print("Usage: python file.py <withdraw_amount> <balance> <pin>")
-    sys.exit()
-if(
-withdraw_amount = float(sys.argv[1])
-balance = float(sys.argv[2])
-pin = sys.argv[3]){
-
-print(f"ATM withdrawal amount is: {withdraw_amount}")
-print(f"Balance is: {balance}")
-print(f"PIN is: {pin}")}
-else{
-withdraw_amount = 500
-balance = 2000
-pin = 636360
-print("withdrawl_amount",withdrawl_amount)
-print("balance",balance)
-print("pin",pin)
-}
 
 
-if balance > withdraw_amount:
-    print("The transaction is approved")
+
+#
+if len(sys.argv) == 4:
+    withdraw_amount = float(sys.argv[1])
+    balance = float(sys.argv[2])
+    pin = sys.argv[3]
+
+    print("User-entered values:")
+    print(f"ATM withdrawal amount is: {withdraw_amount}")
+    print(f"Balance is: {balance}")
+    print(f"PIN is: {pin}")
+
+# Else use default values
 else:
-    print("The transaction is declined")
     
+default_withdraw_amount = 500
+default_balance = 2000
+default_pin = 636360
+
+    print("Default values are used:")
+    print(f"ATM withdrawal amount is: {withdraw_amount}")
+    print(f"Balance is: {balance}")
+    print(f"PIN is: {pin}")
+
+# Transaction condition
+if balance > withdraw_amount:
+    print("\nThe transaction is approved")
+else:
+    print("\nThe transaction is declined")
